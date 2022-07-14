df4fe4205ea722c701154fab54fd0f51a03f2147
Ariel <caminosariel@gmail.com>
2022-07-14 15:40:28 -0300

    add mongo url

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express()
 const port = 5500
+
+const mongoURL = "mongodb+srv://ACaminos:<password>@clustermemes.z97zq.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURL,{useNewUrlParser: true, useUnifiedTopology:true})
+
+app.use(express.json({limit: "100mb"}))
+
+app.post("/api/clients",(req,res)=>{
+    console.log("dumy endPoint")
+    res.send("You have posted something")
+})
 
 app.get("/",(req,res )=>{
     res.send(
